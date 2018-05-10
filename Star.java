38fd382436ed570873f2a40d44df7869cb90ea42
joshuacassidy <Joshcassidy79@gmail.com>
2018-05-10 21:01:20 +0100

    Added custom exeception class

--- Star.java
+++ Star.java
@@ -24,7 +24,7 @@ public class Star implements Network {
                 return v;
             }
         }
-        throw new RuntimeException("Could not find vertex" + destinationName);
+        throw new VertexNotFoundException("Could not find vertex" + destinationName);
     }
 
     @Override
